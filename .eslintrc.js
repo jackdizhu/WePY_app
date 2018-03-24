bb14a898f264ae83f6ba2579bc9448313ca246a4
jackdizhu <376365334@qq.com>
2018-03-24 10:02:13 +0800

    eslint 配置修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,24 +16,12 @@ module.exports = {
   settings: {
     'html/html-extensions': ['.html', '.wpy']
   },
-  'globals': {
-      'wx': true
-  },
   // add your custom rules here
   'rules': {
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
-    "semi": [
-        "error",
-        "always"
-    ],
-    'comma-dangle': ["error", "only-multiline"],
-    'padded-blocks': 0,
-    'one-var': 0,
-    'no-return-assign': 0,
-    'indent': ['error', 4],
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'space-before-function-paren': 0
