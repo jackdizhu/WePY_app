a429c433596c704e4ac4602a7b3bbcbf8c1d2bc3
jackdizhu <376365334@qq.com>
2018-07-26 17:20:53 +0800

    列表页面修改

--- src/pages/list/index.vue
+++ src/pages/list/index.vue
@@ -1,5 +1,40 @@
 <template>
   <div class="page list-page">
+    <div class="goods-sorts">
+      <div class="ul-box">
+        <div class="li-box" v-for="(item, key) in 16" :key="key" hover-class="li-box_active" @click="getGoodsList">粤菜</div>
+      </div>
+    </div>
+    <!-- <div class="list-connent">
+      <div class="page__hd list-page_hd">
+        <div class="page__title">菜谱 商品列表</div>
+        <div class="page__desc">商品分类列表</div>
+      </div>
+      <div class="page__bd list-page_bd">
+        <div class="list-page_bd_box">
+          <scroll-view v-if="listData.length" class="scroll-view-box weui-cells weui-cells_after-title" scroll-y @scrolltoupper="upper" @scrolltolower="lower" @scroll="scroll" :scroll-into-view="toView" :scroll-top="scrollTop">
+            <div @click="navigatorToDetails(item)" class="weui-cell weui-cell_access justify-start" hover-class="weui-cell_active" v-for="(item, key) in listData" :key="key">
+              <div class="weui-cell__hd">
+                <image :src="item.img" class="list-image"></image>
+              </div>
+              <div class="weui-cell__bd list-text">
+                <div class="list-text-h1">{{item.name}}</div>
+                <div class="list-text-h2">好评{{item.praise}} 评价{{item.evaluate}}</div>
+                <div class="list-text-h3 flex">
+                  <div class="text-left flex-1">
+                    <div>￥{{item.price}}起送</div><div>|</div><div>配送费￥{{item.distribution}}</div>
+                  </div>
+                  <div class="text-right">
+                    <div>{{item.distance}}</div><div>|</div><div>{{item.time}}</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </scroll-view>
+        </div>
+
+      </div>
+    </div> -->
     <div class="page__hd list-page_hd">
       <div class="page__title">菜谱 商品列表</div>
       <div class="page__desc">商品分类列表</div>
@@ -66,6 +101,28 @@ export default {
     ...mapMutations({
       setCheckItem: 'SET_CHECKITEM'
     }),
+    catchtouchmove () {
+      return false
+    },
+    getGoodsList () {
+      wx.showLoading({
+        title: '加载中',
+        mask: true
+      })
+      // get 请求
+      this.$request({
+        url: this.$api.test_get_list,
+        type: 'GET',
+        params: {
+          page: this.page
+        }
+      }).then(res => {
+        wx.hideLoading()
+        if (res.data) {
+          this.listData = res.data
+        }
+      })
+    },
     navigatorToDetails (item) {
       this.setCheckItem(item)
       wx.navigateTo({
@@ -168,6 +225,42 @@ export default {
 
 </script>
 <style lang='less'>
+  // page {
+  //   height: 100%;
+  // }
+  .list-page {
+    position: relative;
+    padding-left: 80px;
+    height: 100%;
+    .list-connent {
+      height: 100%;
+      overflow: auto;
+    }
+    .goods-sorts {
+      // position: absolute;
+      position: fixed;
+      left: 0;
+      top: 0;
+      bottom: 0;
+      height: auto;
+      overflow: auto;
+      width: 80px;
+      z-index: 3;
+
+      border-right: 1px solid #eee;
+      background-color: #fff;
+      .li-box {
+        width: 100%;
+        height: 60px;
+        line-height: 60px;
+        text-align: center;
+        border-bottom: 1px solid #eee;
+      }
+      .li-box_active {
+        background-color: #eee;
+      }
+    }
+  }
   .list-image {
     margin-right: 10px;
     vertical-align: middle;
