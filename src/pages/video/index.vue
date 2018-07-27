2723d342db83d488fa517adb924d2a8766d8423f
jackdizhu <376365334@qq.com>
2018-07-27 14:47:42 +0800

    视频组件样式问题处理

--- src/pages/video/index.vue
+++ src/pages/video/index.vue
@@ -6,7 +6,9 @@
     </div>
     <div class="page__bd">
       <div class="section tc">
-        <video id="videoEleId" src="http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400"   enable-danmu danmu-btn controls></video>
+        <div class="video-box">
+          <video id="videoEleId" src="http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400"   enable-danmu danmu-btn controls></video>
+        </div>
         <div class="weui-btn-area">
           <div class="weui-cell weui-cell_input weui-cell_videoText">
             <div class="weui-cell__hd">
@@ -66,5 +68,8 @@ export default {
   .weui-cell_videoText {
     border-top: 1px solid #eee;
     border-bottom: 1px solid #eee;
+  }
+  .video-box {
+    text-align: center;
   }
 </style>
