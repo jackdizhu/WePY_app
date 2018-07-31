5bf6e852900ee47430a2c293108a3b10697cf3f8
jackdizhu <376365334@qq.com>
2018-07-31 10:55:39 +0800

    wx.getUpdateManager 检测版本更新

--- src/main.js
+++ src/main.js
@@ -19,10 +19,54 @@ import '../static/weui/weui_theme.less'
 import mpvueToastRegistry from 'mptoast/registry'
 mpvueToastRegistry(Vue)
 
+let wxHasUpdate = function () {
+  // 获取小程序更新机制兼容
+  if (wx.canIUse('getUpdateManager')) {
+    const updateManager = wx.getUpdateManager()
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调
+      console.log(res, '版本信息')
+
+      if (res.hasUpdate) {
+        updateManager.onUpdateReady(function () {
+          wx.showModal({
+            title: '更新提示',
+            content: '新版本已经准备好，是否重启应用？',
+            success: function (res) {
+              if (res.confirm) {
+                // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+                updateManager.applyUpdate()
+              }
+            }
+          })
+        })
+        updateManager.onUpdateFailed(function () {
+          // 新的版本下载失败
+          wx.showModal({
+            title: '新版本已经上线',
+            content: '已经有新版本了哟，请您删除当前小程序，重新搜索打开'
+          })
+        })
+      }
+    })
+  } else {
+    // 如果希望用户在最新版本的客户端上体验您的小程序，可以这样子提示
+    wx.showModal({
+      title: '提示',
+      content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
+    })
+  }
+}
+
 // 同一路由切换时，上一次的页面数据会保留
 const somePlugin = {
   install: function () {
     Vue.mixin({
+      created () {
+        // 检测版本更新 执行一次
+        wxHasUpdate && wxHasUpdate()
+        wxHasUpdate = null
+      },
       onUnload () {
         if (this.$options.data) {
           Object.assign(this.$data, this.$options.data())
