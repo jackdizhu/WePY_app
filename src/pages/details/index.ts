5635d96657e9aa6ca28cc2fa93b4715662001834
jackdizhu <376365334@qq.com>
2018-08-28 13:08:33 +0800

    优化

--- src/pages/details/index.ts
+++ src/pages/details/index.ts
@@ -133,8 +133,9 @@ export default class Details extends VueClass {
   }
 
   onUnload () {
-    if (this.$options.data) {
-      Object.assign(this.$data, this.$options.data)
-    }
+    this.detailsData = {}
+    // if (this.$options.data) {
+    //   Object.assign(this.$data, this.$options.data)
+    // }
   }
 }
